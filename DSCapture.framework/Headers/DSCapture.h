bf0b2b7ae7daa3e3867e8133dcfaa0806685d2c0
KrishnaMohan454 <ankeshwarkrishna@gmail.com>
2020-06-08 16:39:29 +0530

    Add files via upload
    added header files manually

---
+++ DSCapture.framework/Headers/DSCapture.h
@@ -0,0 +1,19 @@
+//
+//  DSCapture.h
+//  DSCapture
+//
+//  Created by AID011 on 03/06/20.
+//  Copyright © 2020 AID011. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <DSCapture/ModelClass.h>
+//! Project version number for DSCapture.
+FOUNDATION_EXPORT double DSCaptureVersionNumber;
+
+//! Project version string for DSCapture.
+FOUNDATION_EXPORT const unsigned char DSCaptureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSCapture/PublicHeader.h>
+
+
---
+++ DSCapture.framework/Headers/ModelClass.h
@@ -0,0 +1,17 @@
+//
+//  ModelClass.h
+//  DemoFramework
+//
+//  Created by AID011 on 29/05/20.
+//  Copyright © 2020 AID011. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModelClass : NSObject
+
++(void)getName;
+
+@end
+
+
